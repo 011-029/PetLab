d6aae3536d2a2dee0d4ea27a8c30eaf47e5abb0b
Minseo <svt72858@gmail.com>
2025-11-08 19:43:55 +0900

    나윤언니에게 인사하다

--- src/test.java
+++ src/test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hi");
-        System.out.println("냐윤언니 하이")
+        System.out.println("냐윤언니 하이");
     }
 }
