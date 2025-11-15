2b43219004e9aab2ad6e25b595e7d6c65a726c06
Minseo <svt72858@gmail.com>
2025-11-15 14:46:59 +0900

    feat: implement read, print, matches(String kwd) method in MedicalRecord

--- src/core/MedicalRecord.java
+++ src/core/MedicalRecord.java
@@ -2,21 +2,38 @@ package core;
 
 import facade.UIData;
 import mgr.Manageable;
+import util.ReadUtil;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
-// TODO: 클래스 필드, 메서드 작성 (현재는 필수 구현해야 하는 메서드만 자동완성해 둔 상태)
 public class MedicalRecord implements Manageable, UIData {
+    int id;
+    LocalDate date;
+    String hospital;
+    String category;
+    int cost;
 
     public void read(Scanner scan) {
-
+        id = scan.nextInt();
+        date = ReadUtil.readDate(scan);
+        hospital = ReadUtil.readHospital(scan);
+        category = scan.next();
+        cost = scan.nextInt();
     }
 
     public void print() {
-
+        System.out.printf("[%s] %s / %s / %d원 \n",
+                date, hospital, category, cost );
     }
 
     public boolean matches(String kwd) {
+        if(kwd.length() == 0)
+            return true;
+        if(hospital.contains(kwd) || category.contains(kwd) )
+            return true;
+        if(("" + cost).equals(kwd))
+            return true;
         return false;
     }
 
