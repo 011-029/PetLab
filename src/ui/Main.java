dc6ae7f04da92122df4d13cc1d9829f16aff6559
김나윤 <yoon@kyonggi.ac.kr>
2025-11-19 23:31:44 +0900

    design: apply flatlaf

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,11 +1,24 @@
 package ui;
 
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
 import javax.swing.*;
+
+// https://www.formdev.com/flatlaf/
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            FlatMacLightLaf.setGlobalExtraDefaults(
+                    java.util.Collections.singletonMap("@accentColor", "#A4C3DE")
+            );
+            FlatMacLightLaf.setup();
+        } catch (Exception e) {
+            System.out.println("LnF 적용 오류");
+        }
         SwingUtilities.invokeLater(() -> {
             new MainFrame();   // GUI 실행
         });
     }
 }
+
